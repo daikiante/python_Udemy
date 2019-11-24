1dd1a2e229228dd058b9b72063d69ac7c23469d6
daikiante <daikiante@gmail.com>
2019-11-24 23:30:34 +0530

    third

--- lesson_018.py
+++ lesson_018.py
@@ -63,3 +63,101 @@ d = {x: y for x, y in zip(w, f)}
 print(d)
 
 
+# ジェネレーター内包表記
+
+def g():
+    for i in range(10):
+        yield i
+
+g = g()
+
+g = (i for i in range(10))
+
+for x in g:
+    print(x)
+
+
+print('--------------------------')
+
+
+
+'''
+名前空間とスコープ
+
+defの中(local)ではanimalが宣言されていないので、1行目のanimalは宣言していない変数を
+呼び出そうとしているためエラーになる。
+
+'''
+animal = 'cat'
+
+def f():
+    # print(animal)
+    animal = 'dog'
+    print('after:',animal)
+
+f()
+print('global:',animal)
+
+
+print('--------------------------')
+
+
+
+
+
+
+
+'''
+例外処理
+
+本来ならリストlにインデックス5はない。
+try,exceptを使うことでエラーを抜けて
+Don't worryが実行される。
+'''
+
+l = [1,2,3]
+i = 5
+
+try:
+    l[i]
+except:
+    print("Don't worry!")
+
+# exceptが何も実行されなかった場合のみ実行される
+else:
+    print('done')
+
+# finallyは必ず実行される
+finally:
+    print('clean up')
+
+
+
+
+
+
+print('--------------------------')
+
+
+
+
+'''
+独自例外処理
+
+間違っていたとしても、
+次の処理に進んでくれる。
+'''
+
+class UppercaseError(Exception):
+    pass
+
+def check():
+    words = ['APPLE','orange','banana']
+    for word in words:
+        if word.isupper():
+            raise UppercaseError(word)
+
+try:
+    check()
+except UppercaseError as exc:
+    print('This is my fault, Go next')
